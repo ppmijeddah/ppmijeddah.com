3eb115db8a02cbacc0e25c4e50ba9408f90128d8
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-23 20:14:22 +0300

    feat: add daisy ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,19 @@ export default {
           900: "#164b63",
           950: "#083144",
         },
+        accent: {
+          50: "#fdf3f3",
+          100: "#fce4e5",
+          200: "#faced0",
+          300: "#f6abae",
+          400: "#ee7b80",
+          500: "#e25157",
+          600: "#ce343b",
+          700: "#ad282e",
+          800: "#9a272c",
+          900: "#782428",
+          950: "#410e10",
+        },
         secondary: {
           50: "#fdfde8",
           100: "#f9f8ce",
@@ -33,5 +46,19 @@ export default {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#098bb3",
+          secondary: "#b1b720",
+          accent: "#9a272c",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
