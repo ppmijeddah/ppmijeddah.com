4afae94f1a58450fe75146554ce8b44b88355936
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-24 22:53:07 +0300

    fix: seo info

--- src/config.mjs
+++ src/config.mjs
@@ -1,20 +1,20 @@
 export default {
   title: "Homepage",
-  titleSuffix: " | Pacamara ☕",
+  titleSuffix: " | PPMI Jeddah ☕",
   description:
-    "Aromatic brew, pure indulgence, exquisite satisfaction.Default description",
+    "PPMI Jeddah adalah organisasi Mahasiswa Indonesia di Jeddah, Saudi Arabia. Universitas di Jeddah yang berafiliasi dengan kami adalah KAU dan KAUST. Kami berada di naungan PPI Dunia",
   defaultOgImage: "./defaultog.jpg",
   latestPosts: 3,
   postsPerPage: 3,
-  url: "https://pacamara-astro-6y7xr.kinsta.page",
+  url: "https://ppmijeddah.com",
   social: {
     dribbble: false,
-    facebook: false,
-    github: "https://github.com/palmiak",
-    instagram: false,
+    facebook: "https://www.facebook.com/ppmijeddah",
+    github: false,
+    instagram: "https://www.instagram.com/ppmijeddah",
     mastodon: false,
     pinterest: false,
     skype: false,
-    twitter: "https://twitter.com/palmiak_fp",
+    twitter: false,
   },
 };
