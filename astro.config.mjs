77b11d80da866f580649080a0945b928b3f91445
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-27 09:50:03 +0300

    feat: image carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: "https://pacamara-astro-6y7xr.kinsta.page",
+  integrations: [tailwind(), mdx(), react()],
+  site: "https://pacamara-astro-6y7xr.kinsta.page"
 });
---
+++ src/components/general/ImageCarousel.tsx
@@ -0,0 +1,84 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import type { Options } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+
+import "@splidejs/react-splide/css";
+
+const ImageCarousel = () => {
+  const options: Options = {
+    type: "loop",
+    drag: "free",
+    mediaQuery: "min",
+    perPage: 2,
+    gap: "2rem",
+    breakpoints: {
+      1024: {
+        perPage: 3,
+        gap: "3rem",
+      },
+    },
+    arrows: false,
+    pagination: false,
+  };
+
+  return (
+    <Splide
+      aria-label="My Favorite Images"
+      options={options}
+      extensions={{ AutoScroll }}
+    >
+      <SplideSlide>
+        <div className="relative overflow-hidden bg-white dark:bg-pacamara-dark rotate-2 rounded-[15px]">
+          <img
+            src="/slide-1.jpg"
+            width={800}
+            alt=""
+            className="object-cover image-shine h-[200px] md:h-[500px]"
+          />
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="relative overflow-hidden bg-white dark:bg-pacamara-dark rotate-2 rounded-[15px]">
+          <img
+            src="/slide-2.jpg"
+            width={800}
+            alt=""
+            className="object-cover image-shine h-[200px] md:h-[500px]"
+          />
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="relative overflow-hidden bg-white dark:bg-pacamara-dark rotate-2 rounded-[15px]">
+          <img
+            src="/slide-3.jpg"
+            width={800}
+            alt=""
+            className="object-cover image-shine h-[200px] md:h-[500px]"
+          />
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="relative overflow-hidden bg-white dark:bg-pacamara-dark rotate-2 rounded-[15px]">
+          <img
+            src="/slide-4.jpg"
+            width={800}
+            alt=""
+            className="object-cover image-shine h-[200px] md:h-[500px]"
+          />
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="relative overflow-hidden bg-white dark:bg-pacamara-dark rotate-2 rounded-[15px]">
+          <img
+            src="/slide-5.jpg"
+            width={800}
+            alt=""
+            className="object-cover image-shine h-[200px] md:h-[500px]"
+          />
+        </div>
+      </SplideSlide>
+    </Splide>
+  );
+};
+
+export default ImageCarousel;
